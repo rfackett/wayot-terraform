0a445facdf422c6708139f79cd175f1ccc24ac10
rfackett <ryan@foghornconsulting.com>
2018-04-16 08:17:03 -0700

    fmt

--- wayot/us-west-2/search/search.tf
+++ wayot/us-west-2/search/search.tf
@@ -1,5 +1,6 @@
 variable "tags" {
   type = "map"
+
   default = {
     Environment = "wayot"
     Application = "search"
@@ -10,9 +11,10 @@ variable "tags" {
 
 variable "whitelist" {
   type = "list"
+
   default = [
     "69.222.185.243/32",
-    "69.222.185.243/32"
+    "69.222.185.243/32",
   ]
 }
 
@@ -28,20 +30,20 @@ data "aws_subnet_ids" "default" {
 }
 
 module "alb" {
-  source                        = "terraform-aws-modules/alb/aws"
-  load_balancer_name            = "search"
-  security_groups               = ["${var.aws_security_group.search_alb.id}"]
-  log_bucket_name               = "wayot-backups"
-  log_location_prefix           = "alb/search"
-  subnets                       = "${data.aws_subnet_ids.default.ids}"
-  tags                          = "${merge(var.tags, map("Name", "search"), map("Function", "alb"))}"
-  vpc_id                        = "${data.aws_vpc.default.id}"
-  https_listeners               = "${list(map("certificate_arn", "arn:aws:acm:us-west-2:835387872147:certificate/cfe3929a-ea07-4569-b232-fc9e66e2eff3", "port", 443))}"
-  https_listeners_count         = "1"
-  http_tcp_listeners            = "${list(map("port", "80", "protocol", "HTTP"))}"
-  http_tcp_listeners_count      = "1"
-  target_groups                 = "${list(map("name", "search", "backend_protocol", "HTTP", "backend_port", "80"))}"
-  target_groups_count           = "1"
+  source                   = "terraform-aws-modules/alb/aws"
+  load_balancer_name       = "search"
+  security_groups          = ["${var.aws_security_group.search_alb.id}"]
+  log_bucket_name          = "wayot-backups"
+  log_location_prefix      = "alb/search"
+  subnets                  = "${data.aws_subnet_ids.default.ids}"
+  tags                     = "${merge(var.tags, map("Name", "search"), map("Function", "alb"))}"
+  vpc_id                   = "${data.aws_vpc.default.id}"
+  https_listeners          = "${list(map("certificate_arn", "arn:aws:acm:us-west-2:835387872147:certificate/cfe3929a-ea07-4569-b232-fc9e66e2eff3", "port", 443))}"
+  https_listeners_count    = "1"
+  http_tcp_listeners       = "${list(map("port", "80", "protocol", "HTTP"))}"
+  http_tcp_listeners_count = "1"
+  target_groups            = "${list(map("name", "search", "backend_protocol", "HTTP", "backend_port", "80"))}"
+  target_groups_count      = "1"
 }
 
 module "asg" {
@@ -60,20 +62,20 @@ module "asg" {
     {
       volume_size = "100"
       volume_type = "gp2"
-    }
+    },
   ]
 
   # Auto scaling group
-  asg_name                  = "search"
-  vpc_zone_identifier       = "${data.aws_subnet_ids.default.ids}"
-  health_check_type         = "ELB"
-  min_size                  = 0
-  max_size                  = 1
-  desired_capacity          = 1
-  wait_for_capacity_timeout = 0
+  asg_name                    = "search"
+  vpc_zone_identifier         = "${data.aws_subnet_ids.default.ids}"
+  health_check_type           = "ELB"
+  min_size                    = 0
+  max_size                    = 1
+  desired_capacity            = 1
+  wait_for_capacity_timeout   = 0
   associate_public_ip_address = true
-  key_name = "search"
-  load_balancers = ["${module.alb.load_balancer_id}"]
+  key_name                    = "search"
+  load_balancers              = ["${module.alb.load_balancer_id}"]
 
   tags = [
     {
@@ -100,7 +102,7 @@ module "asg" {
       key                 = "Name"
       value               = "search"
       propagate_at_launch = true
-    }
+    },
   ]
 }
 
@@ -132,21 +134,21 @@ resource "aws_security_group_rule" "search_alb_443" {
 }
 
 resource "aws_security_group_rule" "search_alb_80_app" {
-  type        = "egress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  source_security_group_id  = "${aws_security_group.search_lc.id}"
+  type                     = "egress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  source_security_group_id = "${aws_security_group.search_lc.id}"
 
   security_group_id = "${aws_security_group.search_alb.id}"
 }
 
 resource "aws_security_group_rule" "search_alb_443_app" {
-  type        = "egress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  source_security_group_id  = "${aws_security_group.search_lc.id}"
+  type                     = "egress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  source_security_group_id = "${aws_security_group.search_lc.id}"
 
   security_group_id = "${aws_security_group.search_alb.id}"
 }
@@ -159,21 +161,21 @@ resource "aws_security_group" "search_lc" {
 }
 
 resource "aws_security_group_rule" "search_lc_80" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  source_security_group_id  = "${aws_security_group.search_alb.id}"
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  source_security_group_id = "${aws_security_group.search_alb.id}"
 
   security_group_id = "${aws_security_group.search_lc.id}"
 }
 
 resource "aws_security_group_rule" "search_lc_443" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  source_security_group_id  = "${aws_security_group.search_alb.id}"
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  source_security_group_id = "${aws_security_group.search_alb.id}"
 
   security_group_id = "${aws_security_group.search_lc.id}"
 }
@@ -206,11 +208,11 @@ resource "aws_security_group" "search_db" {
 }
 
 resource "aws_security_group_rule" "search_db_3306" {
-  type        = "ingress"
-  from_port   = 3306
-  to_port     = 3306
-  protocol    = "tcp"
-  source_security_group_id  = "${aws_security_group.search_lc.id}"
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  source_security_group_id = "${aws_security_group.search_lc.id}"
 
   security_group_id = "${aws_security_group.search_db.id}"
 }
